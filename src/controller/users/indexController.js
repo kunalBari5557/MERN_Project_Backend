99036ecc1795b8eaf62d6dfbf74c35f7bc3e643d
kunal <kunal21@gmail.com>
2023-08-10 16:07:00 +0530

    feature: search functionality implementation

--- src/controller/users/indexController.js
+++ src/controller/users/indexController.js
@@ -2,26 +2,68 @@
 // const UserSchema = require("../../models/products");
 
 // module.exports.index = async (req, resp) => {
-//   let data = await UserSchema.findAll();
-//   data.forEach(element => {
-//     element.image = element.dataValues.image ? `${req.protocol}://${req.get('host')}/public/image/${element.dataValues.image}` : '';
+//   const page = req.query.page ? parseInt(req.query.page) : 1;
+//   const perPage = req.query.perPage ? parseInt(req.query.perPage) : 10;
+
+//   const offset = (page - 1) * perPage;
+
+//   const totalCount = await UserSchema.count();
+//   const totalPages = Math.ceil(totalCount / perPage);
+
+//   const data = await UserSchema.findAll({
+//     limit: perPage,
+//     offset: offset,
 //   });
-//   resp.send(data);
+
+//   const modifiedData = data.map((element) => {
+//     const imageUrl = element.dataValues.image
+//       ? `${req.protocol}://${req.get("host")}/public/image/${
+//           element.dataValues.image
+//         }`
+//       : "";
+
+//     return {
+//       ...element.dataValues,
+//       image: imageUrl,
+//     };
+//   });
+
+//   resp.send({
+//     data: modifiedData,
+//     pagination: {
+//       currentPage: page,
+//       totalPages: totalPages,
+//       totalCount: totalCount,
+//     },
+//   });
 // };
 
 const Joi = require("joi");
 const UserSchema = require("../../models/products");
+const Sequelize = require("sequelize");
 
 module.exports.index = async (req, resp) => {
   const page = req.query.page ? parseInt(req.query.page) : 1;
   const perPage = req.query.perPage ? parseInt(req.query.perPage) : 10;
+  const searchQuery = req.query.search || ""; // Get the search query from the request
 
   const offset = (page - 1) * perPage;
 
-  const totalCount = await UserSchema.count();
+  const whereCondition = {
+    // Define your search criteria here based on your schema fields
+    title: {
+      [Sequelize.Op.like]: `%${searchQuery}%`,
+    },
+  };
+
+  const totalCount = await UserSchema.count({
+    where: whereCondition, // Apply the search criteria
+  });
+
   const totalPages = Math.ceil(totalCount / perPage);
 
   const data = await UserSchema.findAll({
+    where: whereCondition, // Apply the search criteria
     limit: perPage,
     offset: offset,
   });
