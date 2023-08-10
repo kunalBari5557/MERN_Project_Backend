9819efb46b8c48bd5ae139e2b003c13d6a00ea06
kunal <kunal21@gmail.com>
2023-08-10 10:46:17 +0530

    hotfix/ image url path in product module

--- src/controller/users/indexController.js
+++ src/controller/users/indexController.js
@@ -1,10 +1,50 @@
+// const Joi = require("joi");
+// const UserSchema = require("../../models/products");
+
+// module.exports.index = async (req, resp) => {
+//   let data = await UserSchema.findAll();
+//   data.forEach(element => {
+//     element.image = element.dataValues.image ? `${req.protocol}://${req.get('host')}/public/image/${element.dataValues.image}` : '';
+//   });
+//   resp.send(data);
+// };
+
 const Joi = require("joi");
 const UserSchema = require("../../models/products");
 
 module.exports.index = async (req, resp) => {
-  let data = await UserSchema.findAll();
-  data.forEach(element => {
-    element.image = element.dataValues.image ? `${req.protocol}://${req.get('host')}/public/image/${element.dataValues.image}` : '';
+  const page = req.query.page ? parseInt(req.query.page) : 1;
+  const perPage = req.query.perPage ? parseInt(req.query.perPage) : 10;
+
+  const offset = (page - 1) * perPage;
+
+  const totalCount = await UserSchema.count();
+  const totalPages = Math.ceil(totalCount / perPage);
+
+  const data = await UserSchema.findAll({
+    limit: perPage,
+    offset: offset,
   });
-  resp.send(data);
+
+  const modifiedData = data.map((element) => {
+    const imageUrl = element.dataValues.image
+      ? `${req.protocol}://${req.get("host")}/public/image/${
+          element.dataValues.image
+        }`
+      : "";
+
+    return {
+      ...element.dataValues,
+      image: imageUrl,
+    };
+  });
+
+  resp.send({
+    data: modifiedData,
+    pagination: {
+      currentPage: page,
+      totalPages: totalPages,
+      totalCount: totalCount,
+    },
+  });
 };
